4247775f3845f503ad6fb32640ba45a6301bc299
Saqibur Rahman <85487877+saqibur@users.noreply.github.com>
2022-02-24 10:46:40 +0600

    Add remote logger for version 2 (#2)

--- src/logstack/loggers.py
+++ src/logstack/loggers.py
@@ -1,32 +1,33 @@
-from logstack.handlers import (
-    file_logger,
-    remote_logger,
-)
+import requests
+from .serializers import LogSerializer
 
 
 class Logger(object):
-    def __new__(cls):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super(Logger, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, url: str, port: str, log_name: str) -> None:
+    # TODO: Convert scheme to Enum
+    def __init__(self, scheme: str, url: str, port: str, log_name: str) -> None:
         # TODO: Check if this violates the singleton pattern. I should not be
         #       able to reconfigure the logger after it has been created.
+        self.scheme = scheme
         self.url = url
         self.port = port
         self.log_name = log_name
 
-    def log_to_server():
+    def log_to_remote(self, log: LogSerializer) -> None:
+        log.data['log_name'] = self.log_name
+        requests.post(
+            url=f"{self.scheme}://{self.url}:{self.port}/",
+            json=log.data
+        )
+
+    def log_to_file() -> None:
         raise NotImplementedError
-
-    def log_to_file():
-        raise NotImplementedError
-
-
-class DjangoLogger(Logger):
-    pass
-
 
 class FlaskLogger(Logger):
     pass
